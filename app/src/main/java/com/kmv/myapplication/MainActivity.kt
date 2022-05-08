d801e4925c5fefc32130e32942aaa4c002b75ea8
KMV <9117479@gmail.com>
2022-05-09 01:22:50 +0300

    Projact "Bulletin board" Navigation Drawer 4

--- app/src/main/java/com/kmv/myapplication/MainActivity.kt
+++ app/src/main/java/com/kmv/myapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package com.kmv.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +20,37 @@ class MainActivity : AppCompatActivity() {
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbarAds, R.string.app_open, R.string.app_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.id_my_ads -> {
+                Toast.makeText(this, "Pushed key ${item.itemId.toString()}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_ads_car -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_ads_pc -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_ads_smatphone -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_ads_household_appliance -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_set_ac_sign_up-> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_set_ac_sign_in -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_set_ac_sign_out -> {
+                Toast.makeText(this, "Pushed key ${item.itemId}", Toast.LENGTH_LONG).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
