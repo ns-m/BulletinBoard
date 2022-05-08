d0cdb8f8d597afa70001bc0fc8e8c73fcd27e993
KMV <9117479@gmail.com>
2022-05-09 01:04:39 +0300

    Projact "Bulletin board" Navigation Drawer 3

--- app/src/main/java/com/kmv/myapplication/MainActivity.kt
+++ app/src/main/java/com/kmv/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kmv.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbarAds, R.string.app_open, R.string.app_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
