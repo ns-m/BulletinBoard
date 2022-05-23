b1a7a94d695acd984d02ff6a0e5e39aba0dda083
KMV <9117479@gmail.com>
2022-05-23 23:22:26 +0300

    Project "Bulletin board" 3. EditAсtivity - 2

--- app/src/main/java/com/kmv/myapplication/utils/TreatmentCityList.kt
+++ app/src/main/java/com/kmv/myapplication/utils/TreatmentCityList.kt
@@ -1,4 +1,19 @@
 package com.kmv.myapplication.utils
 
+import android.content.Context
+import java.io.IOException
+import java.io.InputStream
+
 object TreatmentCityList {
+    fun getAllCountries(context: Context):ArrayList<String>{
+        var tempArray = ArrayList<String>()
+        try {
+            val inputStream : InputStream = context.assets.open("countriesToCities.json")
+            val sizeList:Int = inputStream.available()
+            val byteArray = ByteArray(sizeList)
+        }catch (e:IOException){
+
+        }
+        return tempArray
+    }
 }
