71d1452d2904f1dff3b1e78a01c5d20d56e3fcbe
KMV <9117479@gmail.com>
2022-05-24 12:51:42 +0300

    BulletinBoard ActivityEditAdsBinding AlertDialog 1

--- app/src/main/java/com/kmv/myapplication/act/EditAdsAct.kt
+++ app/src/main/java/com/kmv/myapplication/act/EditAdsAct.kt
@@ -12,8 +12,9 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, TreatmentCityList.getAllCountries(this))
+        val listCountry = TreatmentCityList.getAllCountries(this)
+        /*val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, TreatmentCityList.getAllCountries(this))
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.selectCountry.adapter = adapter
+        binding.selectCountry.adapter = adapter*/
     }
 }
---
+++ app/src/main/java/com/kmv/myapplication/dialogs_support/DialogSpinner.kt
@@ -0,0 +1,10 @@
+package com.kmv.myapplication.dialogs_support
+
+import android.app.AlertDialog
+import android.content.Context
+
+class DialogSpinner {
+    fun showSpinnerDialog(context: Context, list:ArrayList<String>){
+        val builder = AlertDialog.Builder(context)
+    }
+}
