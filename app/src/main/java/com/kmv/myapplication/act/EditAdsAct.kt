0fe103a397ebd5a08db8464050718679aebc2dfc
KMV <9117479@gmail.com>
2022-05-24 11:43:49 +0300

    BulletinBoard ActivityEditAdsBinding 2

--- app/src/main/java/com/kmv/myapplication/act/EditAdsAct.kt
+++ app/src/main/java/com/kmv/myapplication/act/EditAdsAct.kt
@@ -2,7 +2,9 @@ package com.kmv.myapplication.act
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.kmv.myapplication.databinding.ActivityEditAdsBinding
+import com.kmv.myapplication.utils.TreatmentCityList
 
 class EditAdsAct : AppCompatActivity() {
     private lateinit var binding: ActivityEditAdsBinding
@@ -10,5 +12,8 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, TreatmentCityList.getAllCountries(this))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.selectCountry.adapter = adapter
     }
 }
--- app/src/main/java/com/kmv/myapplication/utils/TreatmentCityList.kt
+++ app/src/main/java/com/kmv/myapplication/utils/TreatmentCityList.kt
@@ -1,6 +1,7 @@
 package com.kmv.myapplication.utils
 
 import android.content.Context
+import org.json.JSONObject
 import java.io.IOException
 import java.io.InputStream
 
@@ -11,6 +12,15 @@ object TreatmentCityList {
             val inputStream : InputStream = context.assets.open("countriesToCities.json")
             val sizeList:Int = inputStream.available()
             val byteArray = ByteArray(sizeList)
+            inputStream.read(byteArray)
+            val jsonFile: String = String(byteArray)
+            val jsonObject = JSONObject(jsonFile)
+            val coutryName = jsonObject.names()
+            if (coutryName != null){
+            for (unit in 0 until coutryName.length()){
+                tempArray.add(coutryName.getString(unit))
+            }
+            }
         }catch (e:IOException){
 
         }
