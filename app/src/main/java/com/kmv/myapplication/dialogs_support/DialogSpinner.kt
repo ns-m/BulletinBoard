ca020c62821ac5d80001a4c091b96554a5d9353f
KMV <9117479@gmail.com>
2022-05-24 23:50:26 +0300

    Project "Bulletin board" Countries list 2

--- app/src/main/java/com/kmv/myapplication/dialogs_support/DialogSpinner.kt
+++ app/src/main/java/com/kmv/myapplication/dialogs_support/DialogSpinner.kt
@@ -3,12 +3,19 @@ package com.kmv.myapplication.dialogs_support
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.kmv.myapplication.R
 
 class DialogSpinner {
     fun showSpinnerDialog(context: Context, list:ArrayList<String>){
         val builder = AlertDialog.Builder(context)
         val binding = LayoutInflater.from(context).inflate(R.layout.spinner_layout, null)
+        val adapter = RCViewDialogSpinnerAdapter()
+        val rcView = binding.findViewById<RecyclerView>(R.id.spinnerRecyclerView)
+        rcView.layoutManager = LinearLayoutManager(context)
+        rcView.adapter = adapter
+        adapter.updateAdapter(list)
         builder.setView(binding)
         builder.show()
     }
--- app/src/main/java/com/kmv/myapplication/dialogs_support/RCViewDialogSpinnerAdapter.kt
+++ app/src/main/java/com/kmv/myapplication/dialogs_support/RCViewDialogSpinnerAdapter.kt
@@ -1,27 +1,37 @@
 package com.kmv.myapplication.dialogs_support
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kmv.myapplication.R
 
-class RCViewDialogSpinner : RecyclerView.Adapter<RCViewDialogSpinner.SpinnerViewHolder>() {
+class RCViewDialogSpinnerAdapter : RecyclerView.Adapter<RCViewDialogSpinnerAdapter.SpinnerViewHolder>() {
+    private val mainList = ArrayList<String>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpinnerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.spinner_list_item, parent, false)
         return SpinnerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SpinnerViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.setData(mainList[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mainList.size
     }
     class SpinnerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun setData(text : String){
-
+            val tvSpItem = itemView.findViewById<TextView>(R.id.textViewSpinnerList)
+            tvSpItem.text = text
         }
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateAdapter(list: ArrayList<String>){
+        mainList.clear()
+        mainList.addAll(list)
+        notifyDataSetChanged()
     }
 }
