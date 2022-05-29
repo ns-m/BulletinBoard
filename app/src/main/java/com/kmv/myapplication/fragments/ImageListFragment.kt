d81bd1a44fd85e03b2a9feedfcfd28912ce2363b
KMV <9117479@gmail.com>
2022-05-29 17:40:57 +0300

    Project "Bulletin board" Fragment 1

---
+++ app/src/main/java/com/kmv/myapplication/fragments/ImageListFragment.kt
@@ -0,0 +1,16 @@
+package com.kmv.myapplication.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ImageListFragment :  Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
