abd606dd8e653055ff369c6cf252cdd1c63fd82e
KMV <9117479@gmail.com>
2022-06-13 15:21:04 +0300

    BulletinBoard RecyclerView Adapter 2

---
+++ app/src/main/java/com/kmv/myapplication/adapters/AdsRecyclerViewAdapter.kt
@@ -0,0 +1,23 @@
+package com.kmv.myapplication.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdsRecyclerViewAdapter: RecyclerView.Adapter<AdsRecyclerViewAdapter.AdHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AdHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class AdHolder: RecyclerView.ViewHolder() {
+
+    }
+}
