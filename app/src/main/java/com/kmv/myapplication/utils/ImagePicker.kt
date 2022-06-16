08a27f156393eb9344e0ca942930cc2d88e545cc
KMV <9117479@gmail.com>
2022-06-16 17:55:14 +0300

    BulletinBoard Change startActivityForResult 1

--- app/src/main/java/com/kmv/myapplication/utils/ImagePicker.kt
+++ app/src/main/java/com/kmv/myapplication/utils/ImagePicker.kt
@@ -3,7 +3,9 @@ package com.kmv.myapplication.utils
 import android.content.Intent
 import android.graphics.Bitmap
 import android.view.View
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.kmv.myapplication.R
 import com.kmv.myapplication.act.EditAdsAct
@@ -17,9 +19,9 @@ import kotlinx.coroutines.launch
 
 object ImagePicker {
     const val MAX_IMAGE_COUNT = 5
-    const val REQUEST_CODE_GET_IMAGE = 500
-    const val REQUEST_CODE_EDIT_IMAGE = 505
-    fun getOptions(imageCounter: Int): Options{
+    /*const val REQUEST_CODE_GET_IMAGE = 500
+    const val REQUEST_CODE_EDIT_IMAGE = 505*/
+    private fun getOptions(imageCounter: Int): Options{
         val options = Options().apply{
             //ratio = Ratio.RATIO_AUTO                                    //Image/video capture ratio
             count = imageCounter                                        //Number of images to restrict selection count
@@ -32,6 +34,40 @@ object ImagePicker {
             //preSelectedUrls = ArrayList<Uri>()                          //Pre selected Image Urls
         }
         return options
+    }
+
+    fun launchMultiSelectImages(editAA: EditAdsAct, launcher: ActivityResultLauncher<Intent>){
+        PermUtil.checkForCamaraWritePermissions(editAA){
+            val intent = Intent(editAA, Pix::class.java).apply {
+                putExtra("options", getOptions(MAX_IMAGE_COUNT))
+                }
+            launcher.launch(intent)
+        }
+    }
+
+    fun getLauncherForMultiSelectImages(editAA: EditAdsAct): ActivityResultLauncher<Intent>{
+        return editAA.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                if (result.data != null) {
+                    val returnValues = result.data?.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                    if (returnValues?.size!! > 1 && editAA.chooseImageFragment == null) {
+                        editAA.openChooseImageFragment(returnValues)
+                    } else if (returnValues.size == 1 && editAA.chooseImageFragment == null) {
+                        //imageAdapter.update(returnValues)
+                        //val tempList = ImageManager.getImageSize(returnValues[0])
+                        CoroutineScope(Dispatchers.Main).launch{
+                            editAA.binding.progressBarEditAds.visibility = View.VISIBLE
+                            val bitmapArray = ImageManager.imageResize(returnValues) as ArrayList<Bitmap>
+                            editAA.binding.progressBarEditAds.visibility = View.GONE
+                            editAA.imageAdapter.update(bitmapArray)
+                        }
+                    }else if (editAA.chooseImageFragment != null) {
+                        editAA.chooseImageFragment?.updateAdapter(returnValues)
+                    }
+                }
+            }
+        }
     }
 
     fun launcher(edAct:EditAdsAct, launcher: ActivityResultLauncher<Intent>?, imageCounter: Int){
@@ -49,25 +85,7 @@ object ImagePicker {
 
     fun showSelectedImages(resultCode: Int, requestCode: Int, data: Intent?, editAA: EditAdsAct){
 
-        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == REQUEST_CODE_GET_IMAGE) {
-            if (data != null) {
-                val returnValues = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
-                if (returnValues?.size!! > 1 && editAA.chooseImageFragment == null) {
-                    editAA.openChooseImageFragment(returnValues)
-                } else if (returnValues.size == 1 && editAA.chooseImageFragment == null) {
-                    //imageAdapter.update(returnValues)
-                    //val tempList = ImageManager.getImageSize(returnValues[0])
-                    CoroutineScope(Dispatchers.Main).launch{
-                        editAA.binding.progressBarEditAds.visibility = View.VISIBLE
-                        val bitmapArray = ImageManager.imageResize(returnValues) as ArrayList<Bitmap>
-                        editAA.binding.progressBarEditAds.visibility = View.GONE
-                        editAA.imageAdapter.update(bitmapArray)
-                    }
-                }else if (editAA.chooseImageFragment != null) {
-                    editAA.chooseImageFragment?.updateAdapter(returnValues)
-                }
-            }
-        }else if (resultCode == AppCompatActivity.RESULT_OK && requestCode == REQUEST_CODE_EDIT_IMAGE){
+        else if (resultCode == AppCompatActivity.RESULT_OK && requestCode == REQUEST_CODE_EDIT_IMAGE){
             if (data != null) {
                 val uriValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
                 editAA.chooseImageFragment?.setSingleImage(uriValue?.get(0)!!, editAA.editImagePositions)
